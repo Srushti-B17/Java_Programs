4ba1d8713d9e0d0ac3ed2a5643054f7c70e8108a
Srushti Bhadane <131882017+Srushti-B17@users.noreply.github.com>
2023-07-11 19:53:27 +0530

    Add files via upload

---
+++ Factor_Numbers.java
@@ -0,0 +1,22 @@
+public class Factor_Numbers 
+{
+
+  public static void main(String[] args)
+   {
+     int number = 20;
+
+    System.out.print("Factors of " + number + " are: ");
+
+    
+    for (int i = 1; i <= number; ++i) 
+    {
+
+        if (number % i == 0) 
+        {
+        
+            System.out.print(i + " ");
+        }
+    }
+  }
+  
+}
