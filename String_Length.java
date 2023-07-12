057cbf4be259d7d575e7e14e87f7cebb757cc2b7
Srushti Bhadane <131882017+Srushti-B17@users.noreply.github.com>
2023-07-12 20:37:43 +0530

    Add files via upload

---
+++ String_Length.java
@@ -0,0 +1,14 @@
+public class String_Length
+ {
+  public static void main(String[] args) 
+  {
+
+    
+    String greet = "Hello! World";
+    System.out.println("String: " + greet);
+
+   
+    int length = greet.length();
+    System.out.println("Length: " + length);
+  }
+}
