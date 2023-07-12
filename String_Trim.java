b6457449d23e79666f91b8f7cce4f786ae2dd40e
Srushti Bhadane <131882017+Srushti-B17@users.noreply.github.com>
2023-07-12 20:38:27 +0530

    Add files via upload

---
+++ String_Trim.java
@@ -0,0 +1,10 @@
+public class String_Trim {
+  public static void main(String[] args) {
+    String str1 = "   Learn Java Programming      ";
+
+    System.out.println(str1.trim());
+
+  }
+}
+
+// Output: Learn Java Programming
