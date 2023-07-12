4ae8191e2359c66f78fbd175b8f15e0babfd141b
Srushti Bhadane <131882017+Srushti-B17@users.noreply.github.com>
2023-07-12 20:37:25 +0530

    Add files via upload

---
+++ Create_String.java
@@ -0,0 +1,15 @@
+public class Create_String 
+{
+    public static void main(String[] args) {
+      
+      
+      String first = "Java";
+      String second = "Python";
+      String third = "JavaScript";
+  
+      
+      System.out.println(first);  
+      System.out.println(second);  
+      System.out.println(third);   
+    }
+  }
