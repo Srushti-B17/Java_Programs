5ed07c9591cc1a5078f07d2963c2df483b5e17ca
Srushti Bhadane <131882017+Srushti-B17@users.noreply.github.com>
2023-07-14 23:46:37 +0530

    Add files via upload

---
+++ ReplaceString.java
@@ -0,0 +1,15 @@
+class ReplaceString {
+  public static void main(String[] args) {
+    String str1 = "abc cba";
+
+    
+    System.out.println(str1.replace('a', 'z'));  
+
+    
+    System.out.println("Lava".replace('L', 'J'));  
+
+    
+    System.out.println("Hello".replace('4', 'J')); 
+
+  }
+}
