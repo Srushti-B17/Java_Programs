790998ae3e554bee140d3b661b4aec3cc8964efc
Srushti Bhadane <131882017+Srushti-B17@users.noreply.github.com>
2023-07-14 23:46:56 +0530

    Add files via upload

---
+++ IsEmptyString.java
@@ -0,0 +1,11 @@
+class IsEmptyString{
+  public static void main(String[] args) {
+
+    String str1 = "Java Programming";
+    String str2 = "";
+
+    System.out.println(str1.isEmpty()); 
+    System.out.println(str2.isEmpty()); 
+
+  }
+}
