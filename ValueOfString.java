896bca0de803078bf4a2fbcebdf2449b03a47ec1
Srushti Bhadane <131882017+Srushti-B17@users.noreply.github.com>
2023-07-14 23:47:23 +0530

    Add files via upload

---
+++ ValueOfString.java
@@ -0,0 +1,15 @@
+public class ValueOfString {
+  public static void main(String[] args) {
+    int a = 5;
+    long l = -2343834L;
+    float f = 23.4f;
+    double d = 923.234d;
+
+   
+    System.out.println(String.valueOf(a)); 
+    System.out.println(String.valueOf(l));  
+    System.out.println(String.valueOf(f));  
+
+    System.out.println(String.valueOf(d));  
+  }
+}
